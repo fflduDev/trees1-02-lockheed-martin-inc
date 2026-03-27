1731517dab6acde9442cd2d6a8efaf18d1f7eb58
michaelkobus <mtkobus123@gmail.com>
2026-03-27 13:24:22 -0400

    made folder tree

--- tree/OrgChartImpl.java
+++ tree/OrgChartImpl.java
@@ -3,6 +3,8 @@ package tree;
 import java.util.ArrayList;
 import java.util.List;
 
+import tree.GenericTreeNode;
+
 public class OrgChartImpl implements OrgChart{
 
 	//Employee is your generic 'E'..
