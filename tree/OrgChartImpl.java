7dc27eceb60af4a0453ac614cd35b8318602fa73
michaelkobus <mtkobus123@gmail.com>
2026-03-27 13:27:59 -0400

    Added methods to orgchartimpl

--- tree/OrgChartImpl.java
+++ tree/OrgChartImpl.java
@@ -7,8 +7,46 @@ import tree.GenericTreeNode;
 
 public class OrgChartImpl implements OrgChart{
 
+
+	
 	//Employee is your generic 'E'..
 	private List<GenericTreeNode<Employee>> nodes = new ArrayList<>();
+
+	@Override
+	public void addRoot(Employee e) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'addRoot'");
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'clear'");
+	}
+
+	@Override
+	public void addDirectReport(Employee manager, Employee newPerson) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'addDirectReport'");
+	}
+
+	@Override
+	public void removeEmployee(Employee firedPerson) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'removeEmployee'");
+	}
+
+	@Override
+	public void showOrgChartDepthFirst() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'showOrgChartDepthFirst'");
+	}
+
+	@Override
+	public void showOrgChartBreadthFirst() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'showOrgChartBreadthFirst'");
+	}
 	
 	
 }
